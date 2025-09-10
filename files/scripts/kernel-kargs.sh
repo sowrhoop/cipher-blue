66e4eb7f1827b35e0b1ee7e249e5c6405eb5ed4c
sowrhoop <sowrhoopraajb777@gmail.com>
2025-09-09 20:48:25 -0700

    Add kernel arguments script and update configuration for enhanced security

---
+++ files/scripts/kernel-kargs.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+# Append hardened kernel args once, if missing
+kargs=(
+    amd_iommu=force_isolation
+    debugfs=off
+    efi=disable_early_pci_dma
+    extra_latent_entropy
+    gather_data_sampling=force
+    ia32_emulation=0
+    init_on_alloc=1
+    init_on_free=1
+    intel_iommu=on
+    iommu.passthrough=0
+    iommu.strict=1
+    iommu=force
+    ipv6.disable=1
+    kvm.nx_huge_pages=force
+    l1d_flush=on
+    l1tf=full,force
+    lockdown=confidentiality
+    loglevel=0
+    kvm-intel.vmentry_l1d_flush=always
+    mds=full,nosmt
+    mitigations=auto,nosmt
+    module.sig_enforce=1
+    nosmt=force
+    oops=panic
+    page_alloc.shuffle=1
+    pti=on
+    random.trust_bootloader=off
+    random.trust_cpu=off
+    randomize_kstack_offset=on
+    reg_file_data_sampling=on
+    slab_nomerge
+    slub_debug=ZF
+    spec_rstack_overflow=safe-ret
+    spec_store_bypass_disable=on
+    spectre_bhi=on
+    spectre_v2=on
+    tsx=off
+    tsx_async_abort=full,nosmt
+    vsyscall=none
+)
+
+if command -v rpm-ostree >/dev/null 2>&1; then
+  kargs_str=$(IFS=" "; echo "${kargs[*]}")
+  rpm-ostree kargs --append-if-missing="$kargs_str" >/dev/null
+fi
+
